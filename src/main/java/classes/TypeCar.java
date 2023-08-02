820a48d66bad37d7bf11a1996b6ba929dbda33fe
Khametov Bakir <bakir_90@inbox.ru>
2023-08-02 12:32:16 +0300

    Initial commit

---
+++ src/main/java/classes/TypeCar.java
@@ -0,0 +1,5 @@
+package classes;
+
+public enum TypeCar {
+    SEDAN, PICKUP
+}
---
+++ src/main/java/classes/iWiping.java
@@ -0,0 +1,7 @@
+package classes;
+
+public interface iWiping {
+    void wipWindshield();
+    void wipHeadlights();
+    void wipMirrors();
+}
